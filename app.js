5c21489e729c7873eeaafd1791e1d3388cb2a8da
Mateus <me@xmts.cf>
2025-01-15 15:02:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5479,22 +5479,22 @@
   );
 });
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index@1.0.0/css/mdui_v2.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index/css/mdui_v2.min.css">'
 );
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index@1.0.0/css/nexmoe_v2.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index/css/nexmoe_v2.min.css">'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index@1.0.0/js/mdui.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index/js/mdui.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index@1.0.0/js/flv.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index/js/flv.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index@1.0.0/js/DPlayer.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index/js/DPlayer.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index@1.0.0/js/markdown-it.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/xmtscf/Google-Drive-Index/js/markdown-it.min.js"></script>'
 );
 document.write(
   "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}@media screen and (max-width:980px){.mdui-list-item .mdui-text-right{display:none}.mdui-container{width:100%!important;margin:0}}</style>"
